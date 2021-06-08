06d813ef99ba73a7112bb76bd061bf955de7b5c8
roberto <roberto_luongo@live.com>
2021-06-09 01:29:00 +0200

    Aggiunto file risorse

---
+++ main_window7.py
@@ -0,0 +1,24 @@
+import sys
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from mainwindow7 import Ui_MainWindow
+from codfisc.codFiscale import CodFiscale
+
+class MainWindow7(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        #self.show()
+
+        self.label.setText('Ciao')
+
+        self.pushButton.setText('Premimi')
+        self.pushButton.clicked.connect(self.pushButton_clicked)
+
+        #self.dataNas.setDisplayFormat("dd/MM/yyyy")
+
+    def pushButton_clicked(self):
+        self.label.setText('Mi hai premuto !')
+        dt = self.dataNas.date()
+        cf = CodFiscale(self.lnCogn.text(), self.lnNome.text(), self.lnSesso.text(), dt.toPyDate(), self.lnLuogo.text())
+        self.setWindowTitle(cf.stampaCF())
